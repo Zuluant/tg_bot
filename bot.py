45e496976ae35715c015c0dcdbe55e801c1295c9
Vadim <vadikolhovshe@gmail.com>
2022-09-29 16:36:52 +0300

    Added new bot.

---
+++ bot.py
@@ -0,0 +1,27 @@
+import telebot
+from datetime import datetime
+
+
+bot = telebot.TeleBot('5799245966:AAErU-lQCZaTDXHDspDaMzINoe2mir6CBAc')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f'Привет, {message.from_user.first_name}'
+    bot.send_message(message.chat.id, mess)
+
+
+@bot.message_handler()
+def get_user_text(message):
+    if message.text == 'Hello':
+        bot.send_message(message.chat.id, 'И тебе привет!')
+    elif message.text.lower() == 'photo':
+        photo = open('1.webp', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    elif message.text.lower() == 'time':
+        bot.send_message(message.chat.id, datetime.now())
+    else:
+        bot.send_message(message.chat.id, f'Я тебя не понимаю')
+
+
+bot.polling(none_stop=True)
