eab60f9e64f576964c2303d6137aaca5b6cb230a
Vadim <vadikolhovshe@gmail.com>
2022-09-30 16:32:38 +0300

    Added test Friday.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import lessons
 from datetime import datetime
 from telebot import types
 
@@ -9,7 +10,7 @@ bot = telebot.TeleBot('5799245966:AAEy8F_GkpAqXr9la3FiZaXv17EK1n9DhyE')
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f'Привет, {message.from_user.first_name} введите /help'
-    markup = types.ReplyKeyboardMarkup()
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_help = types.KeyboardButton('/help')
     markup.add(button_help)
     bot.send_message(message.chat.id, mess, reply_markup=markup)
@@ -18,15 +19,19 @@ def start(message):
 @bot.message_handler(commands=['help'])
 def help(message):
     mess = f'В этом боте можно узнать какой будет следующий урок, а также получить на него код и ссылку.'
-    bot.send_message(message.chat.id, mess)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    what_lesson_now = types.KeyboardButton('/lesson')
+    markup.add(what_lesson_now)
+    bot.send_message(message.chat.id, mess, reply_markup=markup)
 
 
 @bot.message_handler(commands=['lesson'])
 def lesson(message):
     current_time = datetime.now().strftime('%H:%M:%S')
-    if current_time == '00:46:40':
-        bot.send_message(message.chat.id, 'Сейчас будет биология!')
-    bot.send_message(message.chat.id, current_time)
+    day = datetime.now().strftime('%A')
+    if day == 'Friday':
+
+    bot.send_message(message.chat.id, day)
 
 
 bot.polling(none_stop=True)
