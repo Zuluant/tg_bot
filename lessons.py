eec5ca22d6df95206688b6d5734a2f0b9634f187
Vadim <vadikolhovshe@gmail.com>
2022-10-02 13:23:18 +0300

    Refactored.

--- lessons.py
+++ lessons.py
@@ -1,80 +1,80 @@
 days = {
     'Monday': {
-        830: 'Громадянська освіта',
-        925: f'Фізика \n\nhttps://us04web.zoom.us/j/76766170040?pwd=VCTVN0MPEc72fXa4j2EedqIz8CAhqK.1\n'
+        830: 'Громадянська освіта 08:30',
+        925: f'Фізика 09:25 \n\nhttps://us04web.zoom.us/j/76766170040?pwd=VCTVN0MPEc72fXa4j2EedqIz8CAhqK.1\n'
              f'\nИдентификатор конференции: 798 6745 8647\nКод доступа: 58mWUA',
-        1020: f'Математика \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
+        1020: f'Математика 10:20 \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
              f'\nИдентификатор конференции: 724 7934 9277\nКод доступа: hE0exw',
-        1125: f'Хімія \n\nhttps://us04web.zoom.us/j/74202826485?pwd=0x1pyrMhzOhalPHdVKoL3PEWu0Ruba.1\n'
+        1125: f'Хімія 11:25 \n\nhttps://us04web.zoom.us/j/74202826485?pwd=0x1pyrMhzOhalPHdVKoL3PEWu0Ruba.1\n'
              f'\nИдентификатор конференции: 742 0282 6485\nКод доступа: zKQ758',
-        1230: f'Інформатика \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
+        1230: f'Інформатика 12:30 \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
              f'\nИдентификатор конференции: 724 7934 9277\nКод доступа: hE0exw',
-        1325: f'Захист України \n\nhttps://us05web.zoom.us/j/4629163215?pwd=ZVpZa2NlY0hrZkZMbmdZWU1VcW10dz09\n'
+        1325: f'Захист України 13:25 \n\nhttps://us05web.zoom.us/j/4629163215?pwd=ZVpZa2NlY0hrZkZMbmdZWU1VcW10dz09\n'
              f'\nИдентификатор конференции: 462 916 3215\nКод доступа: iTnr9a',
-        1420: f'Історія України \n\nhttps://us04web.zoom.us/j/74375492088?pwd=bYLsNqCJK2PgVUPnnBSLLXaZoLNHhJ.1\n'
+        1420: f'Історія України 14:20 \n\nhttps://us04web.zoom.us/j/74375492088?pwd=bYLsNqCJK2PgVUPnnBSLLXaZoLNHhJ.1\n'
              f'\nИдентификатор конференции: 743 7549 2088\nКод доступа: uM38JL',
     },
     'Tuesday': {
-        830: f'фізична культура \n\nhttps://us05web.zoom.us/j/83436586849?pwd=djZvTWZpR01IMmNPT1RpaUNlRG9Odz09\n'
+        830: f'фізична культура 08:30 \n\nhttps://us05web.zoom.us/j/83436586849?pwd=djZvTWZpR01IMmNPT1RpaUNlRG9Odz09\n'
              f'\nИдентификатор конференции: 834 3658 6849\nКод доступа: 7tfEwz',
-        925: f'Математика \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
+        925: f'Математика 09:25 \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
              f'\nИдентификатор конференции: 724 7934 9277\nКод доступа: hE0exw',
-        1020: f'Історія України \n\nhttps://us04web.zoom.us/j/74375492088?pwd=bYLsNqCJK2PgVUPnnBSLLXaZoLNHhJ.1\n'
+        1020: f'Історія України 10:20 \n\nhttps://us04web.zoom.us/j/74375492088?pwd=bYLsNqCJK2PgVUPnnBSLLXaZoLNHhJ.1\n'
              f'\nИдентификатор конференции: 743 7549 2088\nКод доступа: uM38JL',
-        1125: f'Біологія і екологія \n\nhttps://us04web.zoom.us/j/77791480233?pwd=AiT7crt6bZn3Q4h0f6a1F9NfipDmlj.1\n'
+        1125: f'Біологія і екологія 11:25 \n\nhttps://us04web.zoom.us/j/77791480233?pwd=AiT7crt6bZn3Q4h0f6a1F9NfipDmlj.1\n'
              f'\nИдентификатор конференции: 777 9148 0233\nКод доступа: C3CZWa',
-        1230: f'Українська література \n\nhttps://us04web.zoom.us/j/73332420536?pwd=LCQ0p4W8nb3fPQkVCPqAtUJW4io8Sk.1\n'
+        1230: f'Українська література 12:30 \n\nhttps://us04web.zoom.us/j/73332420536?pwd=LCQ0p4W8nb3fPQkVCPqAtUJW4io8Sk.1\n'
              f'\nИдентификатор конференции: 733 3242 0536\nКод доступа: 12345',
-        1325: 'Іноземна мова (англійська)',
-        1420: f'Українська мова \n\nhttps://us04web.zoom.us/j/79931845239?pwd=T01McTdXWUJTVGpGeCt5RFhDODRCQT09\n'
+        1325: 'Іноземна мова (англійська) 13:25',
+        1420: f'Українська мова 14:20 \n\nhttps://us04web.zoom.us/j/79931845239?pwd=T01McTdXWUJTVGpGeCt5RFhDODRCQT09\n'
              f'\nИдентификатор конференции: 799 3184 5239\nКод доступа: Xfg71M',
-        1600: f'Індивідуальні заняття з математики \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
+        1600: f'Індивідуальні заняття з математики 16:00 \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
              f'\nИдентификатор конференции: 724 7934 9277\nКод доступа: hE0exw'
     },
     'Wednesday': {
-        830: f'Математика \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
+        830: f'Математика 08:30 \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
              f'\nИдентификатор конференции: 724 7934 9277\nКод доступа: hE0exw',
-        925: f'фізична культура \n\nhttps://us05web.zoom.us/j/83436586849?pwd=djZvTWZpR01IMmNPT1RpaUNlRG9Odz09\n'
+        925: f'фізична культура 09:25 \n\nhttps://us05web.zoom.us/j/83436586849?pwd=djZvTWZpR01IMmNPT1RpaUNlRG9Odz09\n'
              f'\nИдентификатор конференции: 834 3658 6849\nКод доступа: 7tfEwz',
-        1020: f'Всесвітня історія \n\nhttps://us04web.zoom.us/j/76766170040?pwd=VCTVN0MPEc72fXa4j2EedqIz8CAhqK.1\n'
+        1020: f'Всесвітня історія 10:20 \n\nhttps://us04web.zoom.us/j/76766170040?pwd=VCTVN0MPEc72fXa4j2EedqIz8CAhqK.1\n'
              f'\nИдентификатор конференции: 767 6617 0040\nКод доступа: r1ML4C',
-        1125: 'Іноземна мова (англійська)',
-        1230: f'Фізика \n\nhttps://us04web.zoom.us/j/76766170040?pwd=VCTVN0MPEc72fXa4j2EedqIz8CAhqK.1\n'
+        1125: 'Іноземна мова (англійська) 11:25',
+        1230: f'Фізика 12:30 \n\nhttps://us04web.zoom.us/j/76766170040?pwd=VCTVN0MPEc72fXa4j2EedqIz8CAhqK.1\n'
              f'\nИдентификатор конференции: 798 6745 8647\nКод доступа: 58mWUA',
-        1325: f'Українська мова \n\nhttps://us04web.zoom.us/j/79931845239?pwd=T01McTdXWUJTVGpGeCt5RFhDODRCQT09\n'
+        1325: f'Українська мова 13:25 \n\nhttps://us04web.zoom.us/j/79931845239?pwd=T01McTdXWUJTVGpGeCt5RFhDODRCQT09\n'
              f'\nИдентификатор конференции: 799 3184 5239\nКод доступа: Xfg71M',
-        1420: f'Інформатика \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
+        1420: f'Інформатика 14:20 \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
              f'\nИдентификатор конференции: 724 7934 9277\nКод доступа: hE0exw',
     },
     'Thursday': {
-        830: f'Географія \n\nhttps://us05web.zoom.us/j/87568544041?pwd=eklxWi9iNG9PQ2lSSFRKVWM5V0ltZz09\n'
+        830: f'Географія 08:30 \n\nhttps://us05web.zoom.us/j/87568544041?pwd=eklxWi9iNG9PQ2lSSFRKVWM5V0ltZz09\n'
              f'\nИдентификатор конференции: 875 6854 4041\nКод доступа: 6Uck1X',
-        925: f'Математика \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
+        925: f'Математика 09:25 \n\nhttps://us04web.zoom.us/j/72479349277?pwd=aMjDcVi3zzAmrERKhcLwvJH7Uufpyy.1\n'
              f'\nИдентификатор конференции: 724 7934 9277\nКод доступа: hE0exw',
-        1020: f'Біологія і екологія \n\nhttps://us04web.zoom.us/j/77791480233?pwd=AiT7crt6bZn3Q4h0f6a1F9NfipDmlj.1\n'
+        1020: f'Біологія і екологія 10:20 \n\nhttps://us04web.zoom.us/j/77791480233?pwd=AiT7crt6bZn3Q4h0f6a1F9NfipDmlj.1\n'
              f'\nИдентификатор конференции: 777 9148 0233\nКод доступа: C3CZWa',
-        1125: f'Українська мова \n\nhttps://us04web.zoom.us/j/79931845239?pwd=T01McTdXWUJTVGpGeCt5RFhDODRCQT09\n'
+        1125: f'Українська мова 11:25 \n\nhttps://us04web.zoom.us/j/79931845239?pwd=T01McTdXWUJTVGpGeCt5RFhDODRCQT09\n'
              f'\nИдентификатор конференции: 799 3184 5239\nКод доступа: Xfg71M',
-        1230: 'Іноземна мова (англійська)',
-        1325: f'Українська література \n\nhttps://us04web.zoom.us/j/73332420536?pwd=LCQ0p4W8nb3fPQkVCPqAtUJW4io8Sk.1\n'
+        1230: 'Іноземна мова (англійська) 12:30',
+        1325: f'Українська література 13:25 \n\nhttps://us04web.zoom.us/j/73332420536?pwd=LCQ0p4W8nb3fPQkVCPqAtUJW4io8Sk.1\n'
              f'\nИдентификатор конференции: 733 3242 0536\nКод доступа: 12345',
-        1420: f'Історія України \n\nhttps://us04web.zoom.us/j/74375492088?pwd=bYLsNqCJK2PgVUPnnBSLLXaZoLNHhJ.1\n'
+        1420: f'Історія України 14:20 \n\nhttps://us04web.zoom.us/j/74375492088?pwd=bYLsNqCJK2PgVUPnnBSLLXaZoLNHhJ.1\n'
              f'\nИдентификатор конференции: 743 7549 2088\nКод доступа: uM38JL',
-        1515: f'фізична культура \n\nhttps://us05web.zoom.us/j/83436586849?pwd=djZvTWZpR01IMmNPT1RpaUNlRG9Odz09\n'
+        1515: f'фізична культура 15:15 \n\nhttps://us05web.zoom.us/j/83436586849?pwd=djZvTWZpR01IMmNPT1RpaUNlRG9Odz09\n'
              f'\nИдентификатор конференции: 834 3658 6849\nКод доступа: 7tfEwz'
     },
     'Friday': {
-        830: f'Громадянська освіта',
-        925: f'Фізика \n\nhttps://us04web.zoom.us/j/76766170040?pwd=VCTVN0MPEc72fXa4j2EedqIz8CAhqK.1\n'
+        830: f'Громадянська освіта 08:30',
+        925: f'Фізика 09:25 \n\nhttps://us04web.zoom.us/j/76766170040?pwd=VCTVN0MPEc72fXa4j2EedqIz8CAhqK.1\n'
              f'\nИдентификатор конференции: 798 6745 8647\nКод доступа: 58mWUA',
-        1020: f'Зарубіжна література \n\nhttps://us04web.zoom.us/j/5780308192?pwd=VnB3SXUwWVA5dmNoZWxhVEtWT2N1Zz09\n'
+        1020: f'Зарубіжна література 10:20 \n\nhttps://us04web.zoom.us/j/5780308192?pwd=VnB3SXUwWVA5dmNoZWxhVEtWT2N1Zz09\n'
              f'\nИдентификатор конференции: 578 030 8192\nКод доступа: 2022',
-        1125: 'Іноземна мова (англійська)',
-        1230: f'Українська мова \n\nhttps://us04web.zoom.us/j/79931845239?pwd=T01McTdXWUJTVGpGeCt5RFhDODRCQT09\n'
+        1125: 'Іноземна мова (англійська) 11:25',
+        1230: f'Українська мова 12:30 \n\nhttps://us04web.zoom.us/j/79931845239?pwd=T01McTdXWUJTVGpGeCt5RFhDODRCQT09\n'
              f'\nИдентификатор конференции: 799 3184 5239\nКод доступа: Xfg71M',
-        1325: f'Мистецтво \n\nhttps://us05web.zoom.us/j/8385240479?pwd=MjdUTGlxd1J0OWRvVW1sNnNzMXBjQT09\n'
+        1325: f'Мистецтво 13:25 \n\nhttps://us05web.zoom.us/j/8385240479?pwd=MjdUTGlxd1J0OWRvVW1sNnNzMXBjQT09\n'
              f'\nИдентификатор конференции: 838 524 0479\nКод доступа: 12345',
-        1420: f'Географія \n\nhttps://us05web.zoom.us/j/87568544041?pwd=eklxWi9iNG9PQ2lSSFRKVWM5V0ltZz09\n'
+        1420: f'Географія 14:20 \n\nhttps://us05web.zoom.us/j/87568544041?pwd=eklxWi9iNG9PQ2lSSFRKVWM5V0ltZz09\n'
              f'\nИдентификатор конференции: 875 6854 4041\nКод доступа: 6Uck1X'
     },
     'Saturday': {
@@ -88,6 +88,6 @@ days = {
 
 def what_is_lesson_now(day, time):
     for i in days[day]:
-        if (time + 45) <= i:
+        if time <= i:
             return days[day][i]
     return 'Уроки на сегодня закончились!'
